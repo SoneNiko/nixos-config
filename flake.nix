832d59ab1e34dc9ae8859e9ef628281b818521c1
Nikolas Heise <nikolasheise683@gmail.com>
2025-09-09 11:18:27 +0200

    Import home manager

--- flake.nix
+++ flake.nix
@@ -3,6 +3,8 @@
 
     inputs = {
         nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+        home-manager.url = "github:nix-community/home-manager";
+        home-manager.inputs.nixpkgs.follows = "nixpkgs";
     };
 
     outputs = {self, nixpkgs, ...}: {
@@ -11,6 +13,7 @@
                 system = "x86_64-linux";
                 modules = [
                     ./hosts/vm-niko-test/configuration.nix
+                    home-manager.nixosModules.home-manager
                 ];
             };
         };
