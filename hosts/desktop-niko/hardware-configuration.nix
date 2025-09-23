9824938ad067549c7db215988875ab70b718a78b
Nikolas Heise <nikolasheise683@gmail.com>
2025-09-23 19:16:49 +0200

    Application of config change at 1758647809

--- hosts/desktop-niko/hardware-configuration.nix
+++ hosts/desktop-niko/hardware-configuration.nix
@@ -38,4 +38,34 @@
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+
+  hardware.bluetooth.enable = true;
+
+  hardware = {
+    graphics = {
+      enable = true;
+      enable32Bit = true;
+
+      extraPackages = with pkgs; [
+        vaapiVdpau
+        libvdpau-va-gl
+
+        nvidia-vaapi-driver
+      ];
+    };
+    nvidia = {
+      package = config.boot.kernelPackages.nvidiaPackages.beta;
+
+      # This sets nvidia-drm.modeset=1
+      modesetting.enable = true;
+    };
+  };
+  environment.systemPackages = with pkgs; [
+    nvidia-vaapi-driver
+    egl-wayland
+  ];
+
+  services.xserver = {
+    videoDrivers = [ "nvidia" ];
+  };
 }
