a9e74977e34a29bbd3dfb097999d3f673aad2988
Nikolas Heise <nikolasheise683@gmail.com>
2025-11-04 17:31:51 +0100

    feat(configuration.nix): add libreoffice-qt6-fresh to desktop and laptop configurations to enhance document editing capabilities

--- hosts/laptop-niko/configuration.nix
+++ hosts/laptop-niko/configuration.nix
@@ -104,7 +104,7 @@
     discord-canary mesa-demos libva-utils glmark2 vivaldi vivaldi-ffmpeg-codecs
     opencommit freecad localsend lsd bat ripgrep ripgrep-all fzf
     protonup-qt protontricks wine winetricks signal-desktop qemu libvirt
-    dxvk vkd3d-proton pferd mattermost-desktop zotero html2pdf
+    dxvk vkd3d-proton pferd mattermost-desktop zotero html2pdf libreoffice-qt6-fresh
   ];
 
   # Enable Android development environment
